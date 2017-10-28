1666588a17657387f5ad9aaf3cef5b5778bb92bd
Pau Argelaguet <pau@outlook.com>
2017-10-28 12:08:23 +0200

    Documenting implementations

--- projects/project1/scripts/implementations.py
+++ projects/project1/scripts/implementations.py
@@ -9,13 +9,17 @@ THRESHOLD = 1e-12
 def least_squares_gd(y, tx, w, max_iters, gamma):
     """
     Linear regression using gradient descent
+    N = #data points
+    D = #number of variables in input data
+    DG = Degree of the polynomial
 
-    :param y:
-    :param tx:
-    :param w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    :param y: Vector of output variables of size 1xN
+    :param tx: Vector of input variables of size Nx(1+(DG*D))
+    :param w: Vector of initial weights of size 1x(1+(DG*D))
+    :param max_iters: Number of maximum iterations on the loop
+    :param gamma: Step size of the iterative method
+    :return: Vector of weights of size 1x(1+(DG*D)) and
+             Mean Squared Error of the obtained weights
     """
     for i in range(max_iters):
         gradient = compute_gradient(y, tx, w)
@@ -26,16 +30,19 @@ def least_squares_gd(y, tx, w, max_iters, gamma):
 def least_squares_sgd(y, tx, w, batch_size, max_iters, gamma):
     """
     Linear regression using stochastic gradient descent
+    N = #data points
+    D = #number of variables in input data
+    DG = Degree of the polynomial
 
-    :param y:
-    :param tx:
-    :param w:
-    :param batch_size:
-    :param max_iters:
-    :param gamma:
-    :return:
+    :param y: Vector of output variables of size 1xN
+    :param tx: Vector of input variables of size Nx(1+(DG*D))
+    :param w: Vector of initial weights of size 1x(1+(DG*D))
+    :param batch_size: Amount of data points taken on each batch of the iteration
+    :param max_iters: Number of maximum iterations on the loop
+    :param gamma: Step size of the iterative method
+    :return: Vector of weights of size 1x(1+(DG*D)) and
+             Mean Squared Error of the obtained weights
     """
-    """Stochastic gradient descent algorithm."""
     gradient = None
     for i in range(max_iters):
         for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size, num_batches=1):
@@ -47,10 +54,14 @@ def least_squares_sgd(y, tx, w, batch_size, max_iters, gamma):
 def least_squares(y, tx):
     """
     Least squares regression using normal equations
+    N = #data points
+    D = #number of variables in input data
+    DG = Degree of the polynomial
 
-    :param y:
-    :param tx:
-    :return:
+    :param y: Vector of output variables of size 1xN
+    :param tx: Vector of input variables of size Nx(1+(DG*D))
+    :return: Vector of weights of size 1x(1+(DG*D)) and
+             Mean Squared Error of the obtained weights
     """
     txt = np.transpose(tx)
     w = np.linalg.solve(txt.dot(tx), txt.dot(y))
@@ -60,11 +71,15 @@ def least_squares(y, tx):
 def ridge_regression(y, tx, lambda_):
     """
     Ridge regression using normal equations
+    N = #data points
+    D = #number of variables in input data
+    DG = Degree of the polynomial
 
-    :param y:
-    :param tx:
-    :param lambda_:
-    :return:
+    :param y: Vector of output variables of size 1xN
+    :param tx: Vector of input variables of size Nx(1+(DG*D))
+    :param lambda_: Regularization parameter
+    :return: Vector of weights of size 1x(1+(DG*D)) and
+             Mean Squared Error of the obtained weights
     """
     txt = np.transpose(tx)
     w = np.linalg.solve((txt.dot(tx) + lambda_ * 2 * y.shape[0] * np.identity(tx.shape[1])), txt.dot(y))
@@ -74,13 +89,17 @@ def ridge_regression(y, tx, lambda_):
 def logistic_regression(y, tx, w, max_iters, gamma):
     """
     Logistic regression using gradient descent
+    N = #data points
+    D = #number of variables in input data
+    DG = Degree of the polynomial
 
-    :param y:
-    :param tx:
-    :param w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    :param y: Vector of output variables of size 1xN
+    :param tx: Vector of input variables of size Nx(1+(DG*D))
+    :param w: Vector of initial weights of size 1x(1+(DG*D))
+    :param max_iters: Number of maximum iterations on the loop
+    :param gamma: Step size of the iterative method
+    :return: Vector of weights of size 1x(1+(DG*D)) and
+             loss of the weights computed by negative log likelihood
     """
     losses = []
 
@@ -95,33 +114,36 @@ def logistic_regression(y, tx, w, max_iters, gamma):
     return w, losses[-1]
 
 
-def reg_logistic_regression(y, tx, lambda_,  max_iters, gamma):
+def reg_logistic_regression(y, tx, lambda_, w, max_iters, gamma):
     """
     Regularized logistic regression using gradient descent
+    N = #data points
+    D = #number of variables in input data
+    DG = Degree of the polynomial
 
-    :param y:
-    :param tx:
-    :param lambda_:
-    :param w:
-    :param max_iters:
-    :param gamma:
-    :return:
+    :param y: Vector of output variables of size 1xN
+    :param tx: Vector of input variables of size Nx(1+(DG*D))
+    :param lambda_: Regularization parameter
+    :param w: Vector of initial weights of size 1x(1+(DG*D))
+    :param max_iters: Number of maximum iterations on the loop
+    :param gamma: Step size of the iterative method
+    :return: Vector of weights of size 1x(1+(DG*D)) and
+             loss of the weights computed by negative log likelihood
     """
     losses = []
-    w, _= least_squares(y,tx)
-    loss_ls = calculate_loss(y,tx,w)
-    #print("LEAST SQ LOSS{}".format(loss_ls))
+    loss_ls = calculate_loss(y, tx, w)
+    # print("LEAST SQ LOSS{}".format(loss_ls))
     for i in range(max_iters):
         w, loss, grad_norm = learning_by_penalized_gradient(y, tx, w, gamma, lambda_)
         if i % 50 == 0:
             print("Current iteration={}, norm_grad={}, gamma={}".format(i, grad_norm, gamma))
 
         losses.append(loss)
-        #if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < THRESHOLD:
+        # if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < THRESHOLD:
         #    print("out for threshold")
         #    break
 
-        if len(losses)> 100 and losses[-1] > losses[-100]:
-            gamma = gamma/10
+        if len(losses) > 100 and losses[-1] > losses[-100]:
+            gamma = gamma / 10
 
     return w, losses[-1]
