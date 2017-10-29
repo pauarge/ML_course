ed1ac5a9a5f9cac0ac61a45791c28687569efe87
natbolon <natalie.bolonbrun@epfl.ch>
2017-10-29 19:50:50 +0100

    changes in helpers: add calculate_loss_reg

--- projects/project1/scripts/validation.py
+++ projects/project1/scripts/validation.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 from clean_data import standardize, discard_outliers, change_y_to_0, remove_bad_data, change_y_to_1
-from helpers import compute_mse, build_poly, build_k_indices, learning_by_penalized_gradient, calculate_loss
+from helpers import compute_mse, build_poly, build_k_indices, learning_by_penalized_gradient, calculate_loss, \
+    calculate_loss_reg
 from implementations import least_squares, reg_logistic_regression, least_squares_gd
-from plots import cross_validation_visualization
+from plots import cross_validation_visualization, cross_validation_visualization_degree
 
 
 def cross_validation(y, x, k_indices, k, degree, lambda_):
@@ -23,13 +24,14 @@ def cross_validation(y, x, k_indices, k, degree, lambda_):
     tx_train = build_poly(x_train, degree)
     tx_test = build_poly(x_test, degree)
 
-    max_iters = 1000
-    gamma = 0.1
-    w_ini = np.ones(tx_train.shape[1])
+    # max_iters = 5000
+    # gamma = 0.1
+    # w_ini = np.ones(tx_train.shape[1])
     # w, mse_tr = least_squares_gd(y_train, tx_train, w_ini, max_iters, gamma)
-    # w, mse_tr = least_squares(y_train, tx_train)
-    w, loss_tr = reg_logistic_regression(y_train, tx_train, lambda_, w_ini, max_iters, gamma)
-    loss_te = calculate_loss(y_test, tx_test, w)
+    w, mse_tr = least_squares(y_train, tx_train)
+    # w, loss_tr = reg_logistic_regression(y_train, tx_train, lambda_, w_ini, max_iters, gamma)
+
+    loss_te = compute_mse(y_test, tx_test, w)
 
     """
     #lambda_ = 0.00001
@@ -44,13 +46,13 @@ def cross_validation(y, x, k_indices, k, degree, lambda_):
     loss_tr = calculate_loss(y_train, tx_train, w)
     loss_te = calculate_loss(y_test, tx_test, w)
     """
-    return loss_tr, loss_te
+    return mse_tr, loss_te
 
 
 def benchmark_lambda(ys_train, x_train, degree=1, plot_name="PATATA"):
     seed = 3
     k_fold = 4
-    lambdas = np.logspace(-4, 0, 15)
+    lambdas = np.logspace(-4, 0, 5)
     # split data in k fold
     k_indices = build_k_indices(ys_train, k_fold, seed)
 
@@ -79,9 +81,9 @@ def benchmark_lambda(ys_train, x_train, degree=1, plot_name="PATATA"):
 
 
 def benchmark_degrees(ys_train, x_train, lambda_=0.01, plot_name="cross_validation"):
-    seed = 1
+    seed = 3
     k_fold = 4
-    degrees = range(1, 5)
+    degrees = range(1, 16)
     # split data in k fold
     k_indices = build_k_indices(ys_train, k_fold, seed)
     # define lists to store the loss of training data and test data
@@ -92,16 +94,16 @@ def benchmark_degrees(ys_train, x_train, lambda_=0.01, plot_name="cross_validati
         print(i)
         tr, te = 0, 0
         for j in range(k_fold):
-            tmp_tr, tmp_te = cross_validation(ys_train, x_train, k_indices, j, i)
+            tmp_tr, tmp_te = cross_validation(ys_train, x_train, k_indices, j, i, lambda_)
             # print(tmp_tr, j)
             # print(tmp_te, j)
             tr += tmp_tr
             te += tmp_te
-        print("MSE_TEST {} DEGREE {}".format(te / k_fold, i))
+        print("LOSS_TEST {} DEGREE {}".format(te / k_fold, i))
         rmse_tr.append(tr / k_fold)
         rmse_te.append(te / k_fold)
 
-    cross_validation_visualization(degrees, rmse_tr, rmse_te, lambda_, plot_name)
+    cross_validation_visualization_degree(degrees, rmse_tr, rmse_te, lambda_, plot_name)
     # degree_min_te = min(enumerate(rmse_te), key=itemgetter(1))[0] + 1
 
 
