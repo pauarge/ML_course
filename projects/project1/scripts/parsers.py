3a83c0ffadc6813a79c865460add75a6d56897c8
Pau Argelaguet <pau@outlook.com>
2017-10-30 18:42:25 +0100

    Documented parsers

--- projects/project1/scripts/parsers.py
+++ projects/project1/scripts/parsers.py
@@ -7,6 +7,11 @@ DATA_DIR = "../data"
 
 
 def load_data():
+    """
+    Loads datasets into the program. If they exist, it loads the cached .pckl files, it loads the .csv otherwise
+
+    :return: Numpy arrays containing train ys, x, ids and test x, ids
+    """
     print("PARSING TRAIN")
     ys_train, x_train, ids_train = load_pickle_data("ys_train"), load_pickle_data("x_train"), load_pickle_data(
         "ids_train")
@@ -27,6 +32,12 @@ def load_data():
 
 
 def load_pickle_data(filename):
+    """
+    Loads data from .pckl file
+
+    :param filename: Filename ({filename}.pckl) of the file located in the ../tmp directory
+    :return: The object if the file existed, None otherwise
+    """
     path = "../tmp/{}.pckl".format(filename)
     if os.path.exists(path):
         f = open(path, 'rb')
@@ -36,6 +47,13 @@ def load_pickle_data(filename):
 
 
 def dump_pickle_data(obj, filename):
+    """
+    Dumps the given object into a .pckl file
+
+    :param obj: Valid Python object to dump
+    :param filename: Filename of the object (will be save as ../tmp/{filename}.pckl)
+    :return: None
+    """
     path = "../tmp/{}.pckl".format(filename)
     f = open(path, 'wb')
     pickle.dump(obj, f)
@@ -43,7 +61,12 @@ def dump_pickle_data(obj, filename):
 
 
 def load_csv_data(data_path):
-    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    """
+    Loads data and returns y (class labels), tX (features) and ids (event ids)
+
+    :param data_path: Data path of the .csv file
+    :return: Ys, input_data and ids loaded from those files
+    """
     y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=[1])
     x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
     ids = x[:, 0].astype(np.int)
