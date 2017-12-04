cdc568be94b9f98d2e5e1501044d33dd5a543031
Marta <marta@tsf-452-wpa-5-127.epfl.ch>
2017-12-04 20:49:52 +0100

    Tornem a provar

--- projects/project2/project_recommender_system/src/A.py
+++ projects/project2/project_recommender_system/src/A.py
@@ -1,21 +1,23 @@
 from run import run
 
 def main():
-    L_user = [0, 0.001,0.01,0.1,0.2,0.5,1]
-    L_item = [0, 0.001, 0.01, 0.1, 0.2, 0.5, 1]
-    N_features = [10,20,30,35,40,45]
-    p_test = [0.1, 0.2, 0.3, 0.4, 0.5]
-    error = {}
-    minim = 9999
-    for i in L_user:
-        for j in L_item:
-            for k in N_features:
-                for p in p_test:
-                    rmse = run(i,j,k,p)
-                    if rmse < minim:
-                        minim = rmse
-                    error[rmse] = (i,j,k,p)
-    return error, minim
+    # L_user = [0, 0.001,0.01,0.1,0.2,0.5,1]
+    # L_item = [0, 0.001, 0.01, 0.1, 0.2, 0.5, 1]
+    # N_features = [10,20,30,35,40,45]
+    # p_test = [0.1, 0.2, 0.3, 0.4, 0.5]
+    # error = {}
+    # minim = 9999
+    # for i in L_user:
+    #     for j in L_item:
+    #         for k in N_features:
+    #             for p in p_test:
+    #                 rmse = run(i,j,k,p)
+    #                 if rmse < minim:
+    #                     minim = rmse
+    #                 error[rmse] = (i,j,k,p)
+    # return error, minim
+    rmse = run(0.001, 0.001, 30, 0.2)
+    print(rmse)
 
 if __name__ == '__main__':
     main()
