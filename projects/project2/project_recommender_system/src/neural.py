a1991a45386054ba0fcf1d82da936ef8ebfe82ad
natbolon <natalie.bolonbrun@epfl.ch>
2017-12-10 17:12:08 +0100

    basic implementation of neural net

--- projects/project2/project_recommender_system/src/neural.py
+++ projects/project2/project_recommender_system/src/neural.py
@@ -19,10 +19,11 @@ class NNLinearRegression(nn.Module):
     def forward(self, x):
         return self.linear(x)
 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        #self.fc1 = nn.Linear(2,1)
+        # self.fc1 = nn.Linear(2,1)
         self.conv1 = nn.Conv2d(2, 2, 2)  # 1 single channel per input, 6 channels per output and 5x5 filters
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(2, 4, 5)
@@ -31,10 +32,10 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(2, 1)  # the final 5 is because there are 5 possibilities (1-5)
 
     def forward(self, x):
-        #x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        #x = F.relu(self.conv2(x))
-        x = self.pool(x)
+        # x = F.relu(self.conv1(x))
+        # x = self.pool(x)
+        # x = F.relu(self.conv2(x))
+        # x = self.pool(x)
         # x = x.view(-1, 16 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -57,22 +58,28 @@ def train(features, labels, model, lossfunc, optimizer, num_epoch):
         loss.backward()
         optimizer.step()
 
-        print('Epoch [%d/%d], Loss: %.4f' % (epoch + 1, num_epoch, loss.data[0]))
+        print('Epoch [%d/%d], Loss: %.8f' % (epoch + 1, num_epoch, np.math.sqrt(loss.data[0])))
 
 
 def main():
-    elems_tr, ratings_tr, elems_ts, ratings_te = load_data_2()
+    elems_tr, ratings_tr, elems_ts, ratings_ts = load_data_2()
 
-    lossfunct = nn.MSELoss()
+    lossfunc = nn.MSELoss()
     model = Net()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 
     train(features=elems_tr,
           labels=ratings_tr,
           model=model,
-          lossfunc=lossfunct,
+          lossfunc=lossfunc,
           optimizer=optimizer,
-          num_epoch=1000)
+          num_epoch=250)
+
+    inputs_ts = Variable(torch.from_numpy(elems_ts))
+    target_ts = Variable(torch.from_numpy(ratings_ts))
+    output = model(inputs_ts)
+    loss = lossfunc(output, target_ts)
+    print("Error Test{}".format(loss))
 
 
 if __name__ == '__main__':
