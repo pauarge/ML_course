a937bec09404d8dd28bb33391f9dc0d70712dedb
Pau Argelaguet <pau@outlook.com>
2017-12-17 18:20:22 +0100

    Adding caching for loading files

--- projects/project2/project_recommender_system/src/parsers.py
+++ projects/project2/project_recommender_system/src/parsers.py
@@ -1,28 +1,64 @@
 from datetime import datetime
 import pandas as pd
 import csv
+import os
+import pickle
 
 from surprise import Reader, Dataset
 
 DATA_DIR = "../data"
+TMP_DIR = "../tmp"
 
 
 def load_data(filename):
-    data = read_txt("{}/{}".format(DATA_DIR, filename))[1:]
-    df = preprocess_data(data)
-    reader = Reader(rating_scale=(1, 5))
-    return Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
+    ds = load_pickle_data("ds")
+    if ds is None:
+        data = load_csv_data("{}/{}".format(DATA_DIR, filename))[1:]
+        df = preprocess_data(data)
+        reader = Reader(rating_scale=(1, 5))
+        ds = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
+        dump_pickle_data(ds, "ds")
+    return ds
 
 
-def read_txt(path):
+def load_csv_data(path):
     """read text file from path."""
     with open(path, "r") as f:
         return f.read().splitlines()
 
 
+def load_pickle_data(filename):
+    """
+    Loads data from .pckl file
+
+    :param filename: Filename ({filename}.pckl) of the file located in the ../tmp directory
+    :return: The object if the file existed, None otherwise
+    """
+    path = "../tmp/{}.pckl".format(filename)
+    if os.path.exists(path):
+        print("LOADING PCKL FILE FROM {}".format(path))
+        f = open(path, 'rb')
+        obj = pickle.load(f)
+        f.close()
+        return obj
+
+
+def dump_pickle_data(obj, filename):
+    """
+    Dumps the given object into a .pckl file
+
+    :param obj: Valid Python object to dump
+    :param filename: Filename of the object (will be save as ../tmp/{filename}.pckl)
+    :return: None
+    """
+    path = "../tmp/{}.pckl".format(filename)
+    f = open(path, 'wb')
+    pickle.dump(obj, f)
+    f.close()
+
+
 def preprocess_data(data):
     """preprocessing the text data, conversion to numerical array format."""
-
     def deal_line(line):
         pos, rating = line.split(',')
         row, col = pos.split("_")
@@ -54,7 +90,7 @@ def create_submission(algo):
         col = col.replace("c", "")
         return row, col
 
-    data = read_txt("{}/sample_submission.csv".format(DATA_DIR))[1:]
+    data = load_csv_data("{}/sample_submission.csv".format(DATA_DIR))[1:]
     cells = [deal_line(line) for line in data]
 
     with open("{}/submission-{}.csv".format(DATA_DIR, datetime.now()), 'w') as csvfile:
--- projects/project2/project_recommender_system/src/run.py
+++ projects/project2/project_recommender_system/src/run.py
@@ -1,6 +1,9 @@
 from surprise import NMF
+from datetime import datetime
 
-from parsers import create_submission, load_data
+from surprise.dump import dump
+
+from parsers import create_submission, load_data, TMP_DIR
 
 
 def main():
@@ -9,11 +12,14 @@ def main():
 
     print("TRAINING MODEL")
     trainset = data.build_full_trainset()
-    algo = NMF(n_factors=25, n_epochs=350, biased=True, verbose=True)
+    algo = NMF(n_factors=15, n_epochs=250, biased=True, verbose=True)
     algo.train(trainset)
 
     print("CREATING SUBMISSION")
     create_submission(algo)
+
+    print("SAVING MODEL")
+    dump("{}/suprise-{}.pckl".format(TMP_DIR, datetime.now()), algo=algo)
 
 
 if __name__ == '__main__':
