d03897f45a9dc218d9821961174484c8eea045a5
Pau Argelaguet <pau@outlook.com>
2017-12-18 19:14:16 +0100

    Documenting source files

--- projects/project2/project_recommender_system/src/run.py
+++ projects/project2/project_recommender_system/src/run.py
@@ -7,14 +7,22 @@ from surprise.dump import dump
 from parsers import create_submission, load_data, TMP_DIR
 
 
+def parse_args():
+    """
+    Sets up a parser for CLI options.
+
+    :return: arguments list
+    """
+    parser = argparse.ArgumentParser(description='Main program for getting rating predictions of the given dataset.')
+    parser.add_argument('-e', '--epochs', default=500, type=int, help="Number of epochs to run.")
+    parser.add_argument('-v', '--verbose', default=True, type=bool, help="Enable verbosity of training.")
+    parser.add_argument('-b', '--biased', default=False, type=bool, help="Run bias on the method.")
+    parser.add_argument('-f', '--factors', default=25, type=int, help="Number of factors of the method.")
+    return parser.parse_args()
+
+
 def main():
-    print("PARSING CMD ARGS")
-    parser = argparse.ArgumentParser(description='Cross validation for movie ratings.')
-    parser.add_argument('--epochs', '-e', default=500, type=int, help="Number of epochs to test.")
-    parser.add_argument('--verbose', '-v', default=True, type=bool, help="Set verbosity of ")
-    parser.add_argument('--biased', '-b', default=False, type=bool, help="Run the method biased")
-    parser.add_argument('--factors', '-f', default=25, type=int, help="Number of factors")
-    args = parser.parse_args()
+    args = parse_args()
 
     print("LOADING DATAFRAME")
     data = load_data("data_train.csv")
