243cfb12511636bbb4bff9909cf2ce881cf8667d
natbolon <natalie.bolonbrun@epfl.ch>
2017-12-20 21:30:49 +0100

    SVD documented

--- projects/project2/project_recommender_system/src_old/utils/SVD.py
+++ projects/project2/project_recommender_system/src_old/utils/SVD.py
@@ -7,6 +7,12 @@ from utils.methods import compute_error_SVD, global_mean, compute_std, standardi
 
 
 def computesvd(train, K):
+    """
+    Computes SVD decomposition with scipy implementation
+    :param train: Matrix of ratings from train set
+    :param K: Number of features
+    :return: Item and users features, mean and standard deviation
+    """
     mean = global_mean(train)
     train = standardize(train, mean)
     std = compute_std(train)
@@ -30,13 +36,15 @@ def computesvd(train, K):
 
 
 def SVD_SGD(train, test, lambda_user, lambda_item, num_features):
-    """matrix factorization by SGD."""
-    # define parameters
-    gamma = 0.01
-    # num_features = 30  # K in the lecture notes
-    # lambda_user = 0.1
-    # lambda_item = 0.01
-    num_epochs = 30  # number of full passes through the train set
+    """
+    Update of SVD matrices
+    :param train: Matrix of ratings from train set
+    :param test: Matrix of ratings from test set
+    :param lambda_user: Float value for the regularization term of the users
+    :param lambda_item: Float value for the regularization term of the items
+    :param num_features: Number of features
+    :return: Item and users features, RMSE on test set.
+    """
 
     # set seed
     np.random.seed(988)
@@ -47,9 +55,7 @@ def SVD_SGD(train, test, lambda_user, lambda_item, num_features):
     pred = user_features.dot(item_features) * std + mean
     print("GENERATE USER AND ITEM FEATURES")
 
-    # # find the non-zero ratings indices
-    # nz_row, nz_col = train.nonzero()
-    # nz_train = list(zip(nz_row, nz_col))
+    # find the non-zero ratings indices
     nz_row, nz_col = test.nonzero()
     nz_test = list(zip(nz_row, nz_col))
 
